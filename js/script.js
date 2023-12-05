0defba2f1f0da313be80562bb5e52243e56e7fae
Marwa Bayood <marwa.bayood96@gmail.com>
2023-12-05 10:27:35 +0200

    create section latest news create section email signup create file json to change language from english to arabic create css rtl to change design when site change language

--- js/script.js
+++ js/script.js
@@ -19,17 +19,33 @@ $(document).ready(function(){
   });
 
   /***************End Count up********************** */
+
+  $("#arabic").trigger("click");
     
   });
 
+  // ******************* Button top up ***********************//
   const toTop = document.querySelector(".to-top");
   window.onscroll = function(){
   //  console.log(this.scrollY);
-   if(this.scrollY >= 900){
+   if(this.scrollY >= 800){
     toTop.classList.add("active");
   }else{
     toTop.classList.remove("active");
   }
   }
+  // ******************* End Button top up *****************//
 
 
+  // *******************Button Translate Localization *****************//
+  function english(){
+    document.getElementById("translatecss1").setAttribute("href","css/custom.css");
+    document.getElementById("translatecss2").setAttribute("href","https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css");
+    document.getElementById("translatejs").setAttribute("src","https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js");
+    }
+    function arabic(){
+    document.getElementById("translatecss1").setAttribute("href","css/customArabic.css");
+    document.getElementById("translatecss2").setAttribute("href","https://cdn.rtlcss.com/bootstrap/v4.1.3/css/bootstrap.min.css");
+    document.getElementById("translatejs").setAttribute("src","https://cdn.rtlcss.com/bootstrap/v4.1.3/js/bootstrap.min.js");
+    }
+// *******************Button Translate Localization *******************//
