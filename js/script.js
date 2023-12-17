e8d34bfadb1c6eaff4af8c448ef98065fc7a2bbf
Marwa Bayood <marwa.bayood96@gmail.com>
2023-12-17 13:32:43 +0200

    final home page updated

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
+
+document.addEventListener("DOMContentLoaded", function(){
+		
+  window.addEventListener('scroll', function() {
+       
+    if (window.scrollY > 50) {
+      document.getElementById('navbar_top').classList.add('fixed-top');
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector('.navbar').offsetHeight;
+      document.body.style.paddingTop = navbar_height + 'px';
+      document.getElementById("navbar_top").classList.add("bg-nav-dark");
+      document.getElementById("navbar_top").classList.remove("navbar-light");
+      document.getElementById("navbar_top").classList.remove("navbg");
+
+    } else {
+       document.getElementById('navbar_top').classList.remove('fixed-top');
+       // remove padding top from body
+      document.body.style.paddingTop = '0';
+      document.getElementById("navbar_top").classList.add("navbar-light");
+      document.getElementById("navbar_top").classList.add("navbg");
+      document.getElementById("navbar_top").classList.remove("bg-nav-dark");
+
+    } 
+  });
+}); 
 $(document).ready(function(){
 
-
-
-  $(".xp-menubar").on('click',function(){
-    $('#sidebar').toggleClass('active');
-    $('#content').toggleClass('active');
+  /***********Ripple water***************/
+  $('main').ripples({
+    resolution: 512,
+    dropRadius: 12, //px
+    perturbance: 0.05,
   });
-  
-   $(".xp-menubar,.body-overlay").on('click',function(){
-     $('#sidebar,.body-overlay').toggleClass('show-nav');
-   });
+  /*************End Ripple water***********/
 
 
    /***************start Count up********************** */
@@ -40,12 +62,29 @@ $(document).ready(function(){
   // *******************Button Translate Localization *****************//
   function english(){
     document.getElementById("translatecss1").setAttribute("href","css/custom.css");
-    document.getElementById("translatecss2").setAttribute("href","https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css");
-    document.getElementById("translatejs").setAttribute("src","https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js");
+    document.getElementById("translatecss2").setAttribute("href","https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css");
+    document.getElementById("translatejs").setAttribute("src","https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js");
+    document.getElementById("form-navbar").classList.add("me-auto");
+    document.getElementById("flags-icons").style.float= "right";
+    document.getElementById("eng-Name").style.textAlign = "right";
+    document.getElementById("btnemail").classList.add("fa-share");
+    document.getElementById("btnemail").classList.remove("fa-reply");
+    // document.getElementById("emailSignup").style.direction = "ltr";
+
     }
     function arabic(){
     document.getElementById("translatecss1").setAttribute("href","css/customArabic.css");
-    document.getElementById("translatecss2").setAttribute("href","https://cdn.rtlcss.com/bootstrap/v4.1.3/css/bootstrap.min.css");
-    document.getElementById("translatejs").setAttribute("src","https://cdn.rtlcss.com/bootstrap/v4.1.3/js/bootstrap.min.js");
-    }
+    document.getElementById("translatecss2").setAttribute("href","https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css");
+    document.getElementById("translatejs").setAttribute("src","https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js");
+    document.getElementById("ul-navbar").classList.add("me-auto");
+    document.getElementById("flags-icons").style.float= "left";
+    document.getElementById("eng-Name").style.textAlign = "left"
+    document.getElementById("btnemail").classList.add("fa-reply");
+    document.getElementById("btnemail").classList.remove("fa-share");
+    // document.getElementById("emailSignup").style.direction = "rtl";
+
+
+
+
+  }
 // *******************Button Translate Localization *******************//
